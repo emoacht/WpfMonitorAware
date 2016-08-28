f7d0385e65e27e26ec383a02c2d220cbbd1d52d7
emoacht <emotom@pobox.com>
2016-08-29 06:38:13 +0900

    Modified test project

--- WpfBuiltinScaling/Views/MainWindow.xaml.cs
+++ WpfBuiltinScaling/Views/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,6 +19,51 @@ namespace WpfBuiltinScaling.Views
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		#region Property
+
+		public string Message
+		{
+			get { return (string)GetValue(MessageProperty); }
+			set { SetValue(MessageProperty, value); }
+		}
+		public static readonly DependencyProperty MessageProperty =
+			DependencyProperty.Register(
+				"Message",
+				typeof(string),
+				typeof(MainWindow),
+				new PropertyMetadata(null));
+
+		#endregion
+
+		private EventHandler<MonitorAware.Models.DpiChangedEventArgs> _onDpiChanged;
+
+		protected override void OnSourceInitialized(EventArgs e)
+		{
+			base.OnSourceInitialized(e);
+
+			_onDpiChanged = (_sender, _e) =>
+			{
+				Message = "MonitorAware Scaling Fired";
+				SystemSounds.Exclamation.Play();
+			};
+			MonitorProperty.WindowHandler.DpiChanged += _onDpiChanged;
+		}
+
+		protected override void OnClosed(EventArgs e)
+		{
+			base.OnClosed(e);
+
+			MonitorProperty.WindowHandler.DpiChanged -= _onDpiChanged;
+		}
+
+		protected override void OnDpiChanged(DpiScale oldDpi, DpiScale newDpi)
+		{
+			base.OnDpiChanged(oldDpi, newDpi);
+
+			Message = "Built-in Scaling Fired";
+			SystemSounds.Hand.Play();
 		}
 	}
 }
