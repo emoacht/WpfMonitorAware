802a1571a6ffa9b7b07feceb0195e8e79957ec5a
emoacht <emotom@pobox.com>
2022-01-05 21:37:57 +0900

    Reconfigure project files for .NET 6.0

--- Source/WpfSlateWindow/Properties/Resources.Designer.cs
+++ Source/WpfSlateWindow/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace WpfSlateWindow.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -61,12 +61,12 @@ namespace WpfSlateWindow.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
-        internal static System.Drawing.Bitmap fourcolors {
+        internal static byte[] fourcolors {
             get {
                 object obj = ResourceManager.GetObject("fourcolors", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
+                return ((byte[])(obj));
             }
         }
     }
